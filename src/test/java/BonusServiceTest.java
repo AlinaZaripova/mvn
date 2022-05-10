bfdc1f5165bf098e36eb6ded286326c79325bc9e
Алина Зарипова <khandyshka.92@mail.ru>
2022-05-11 00:01:25 +0300

    fixed bonus service test

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -41,7 +41,7 @@ public class BonusServiceTest {
         // подготавливаем данные:
         long amount = 1000_60;
         boolean registered = false;
-        long expected = 1;
+        long expected = 10;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
@@ -57,54 +57,6 @@ public class BonusServiceTest {
         // подготавливаем данные:
         long amount = 1_000_000_60;
         boolean registered = false;
-        long expected = 500;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForRegisteredAndOverLimit() {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 0;
-        boolean registered = true;
-        long expected = 0;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForNotRegisteredAndOverLimit() {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 0;
-        boolean registered = false;
-        long expected = 0;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(amount, registered);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.jupiter.api.Test
-    void shouldCalculateForRegisteredAndOverLimit() {
-        BonusService service = new BonusService();
-
-        // подготавливаем данные:
-        long amount = 1_000_000_60;
-        boolean registered = true;
         long expected = 500;
 
         // вызываем целевой метод:
